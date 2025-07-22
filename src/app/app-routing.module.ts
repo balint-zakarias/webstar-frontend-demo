a1a66cc79f75a79c5cd01f1bdd12197bf0faf81b
Balint Zakarias <balint.zakarias@gmail.com>
2025-07-22 11:21:41 +0200

    wip
    generate core, shared, auth and character modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'character', loadChildren: () => import('./character/character.module').then(m => m.CharacterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
